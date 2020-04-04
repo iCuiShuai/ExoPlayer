4a58c8de12052497b04172d3e722194e8272f16c
liwei <wei.li@zenjoy.net>
2020-04-04 16:42:07 +0800

    [LW]fix: add ad intercept and display load control class.

---
+++ extensions/ima/src/main/java/com/google/android/exoplayer2/ext/ima/IAdsIntercept.java
@@ -0,0 +1,15 @@
+package com.google.android.exoplayer2.ext.ima;
+
+import android.view.ViewGroup;
+
+public interface IAdsIntercept {
+    boolean skipAd(int adGroupCounts, int adGroupIndex);
+
+    boolean handleAdError(ImaAdsLoader imaAdsLoader, ViewGroup adContainer, Object adError, int adGroupIndex);
+
+    boolean isPlayingAd();
+
+    boolean handlePlayerError(Throwable throwable);
+
+    void release();
+}
