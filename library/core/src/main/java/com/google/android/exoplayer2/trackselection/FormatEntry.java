9660131520c7cca706b3feb2d74781aa6339eff0
liwei <wei.li@zenjoy.net>
2020-04-23 19:17:10 +0800

    [LW]fix: add audio and subtitle sort rules.

--- library/core/src/main/java/com/google/android/exoplayer2/trackselection/FormatEntry.java
+++ library/core/src/main/java/com/google/android/exoplayer2/trackselection/FormatEntry.java
@@ -1,11 +1,14 @@
 package com.google.android.exoplayer2.trackselection;
 
 import com.google.android.exoplayer2.Format;
+import java.util.List;
 
 public class FormatEntry implements Comparable<FormatEntry> {
     private Format format;
     private int groupIndex;
     private int trackIndex;
+    private List<String> sortList;
+    private boolean video = true;
 
     public static boolean equals(FormatEntry first, FormatEntry second)
     {
@@ -34,8 +37,16 @@ public class FormatEntry implements Comparable<FormatEntry> {
         this.trackIndex = trackIndex;
     }
 
+    public void setSortList(List<String> sortList) {
+        this.sortList = sortList;
+        video = false;
+    }
+
     @Override
     public int compareTo(FormatEntry o) {
+        if (!video) {
+            return compareAudioOrSubtitle(o);
+        }
         if (format.height > o.format.height) {
             return 1;
         }
@@ -49,5 +60,17 @@ public class FormatEntry implements Comparable<FormatEntry> {
             return -1;
         }
         return 0;
+    }
+
+    private int compareAudioOrSubtitle(FormatEntry o) {
+        int current = sortList.indexOf(format.language);
+        int next = sortList.indexOf(o.format.language);
+        if (current == next) {
+            return 0;
+        } else if (current > next) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 }
