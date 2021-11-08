6b678810181f9cdc1340f6edbcc6643408a39a9e
sprakhar001 <prakhar.saxena@mxplayer.in>
2021-11-08 17:54:10 +0530

    [PS] fix html companion visibility

--- extensions/mxInteractiveMediaAds/src/main/java/com/mxplay/interactivemedia/internal/core/CompanionHtmlResourceRenderer.kt
+++ extensions/mxInteractiveMediaAds/src/main/java/com/mxplay/interactivemedia/internal/core/CompanionHtmlResourceRenderer.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Build
-import android.view.View
 import android.view.ViewGroup
 import android.webkit.*
 import com.mxplay.interactivemedia.api.AdEvent
@@ -44,7 +43,6 @@ class CompanionHtmlResourceRenderer(val ioOpsScope: CoroutineScope, private val 
     private fun render(companionInfo: AdCompanionInfo) {
         val companionAd = companionInfo.companionAd as CompanionAdData
         val companionContainer = companionInfo.companionAdSlot.container
-        companionContainer.visibility = View.VISIBLE
         val resource = companionAd.resourceValue
         val width = px2dp(companionAd.width, companionContainer.context)
         val height = px2dp(companionAd.height, companionContainer.context)
